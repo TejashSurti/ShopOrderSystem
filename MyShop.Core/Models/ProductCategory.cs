1435c3e63886fa07098cca0c9c223072347fce67
Tejash Surti <tejas.surti@gmail.com>
2022-04-12 16:29:49 -0400

    Product Grouping added Category list dropdown

---
+++ MyShop.Core/Models/ProductCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class ProductCategory
+    {
+        public string Id { get; set; }
+
+        [DisplayName("Product Category Name")]
+        public string Category { get; set; }
+
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}

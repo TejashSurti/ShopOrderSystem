aa84c97e62a043825470e63e6165dd335123a3af
Tejash Surti <tejas.surti@gmail.com>
2022-04-25 16:24:50 -0400

    SQL

---
+++ MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection"){
+            
+        }
+
+        public DbSet<Product> Products { get; set; } //db set specifies what models we want to store in DB
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
+
